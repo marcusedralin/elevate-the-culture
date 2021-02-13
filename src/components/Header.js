53f1b2aba5f4f27450a9f9d4229ccd52a2e1bc51
Marcus Edralin <medralin36@yahoo.com>
2021-02-12 17:45:14 -0800

    Mission rough

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,28 +7,31 @@ export default class Header extends Component {
         return (
             <>
                 <header className="banner">
-
+                    <Container>
                             <Row>
                                 <Col>
                                     <h1>Elevate<br />the<br/>Culture</h1>
                                 </Col>
-                            </Row>                             
-                                    <Nav>
-                                        <Navbar sticky="top">
-                                            <ul className="main-nav">
-                                               <a className="navbar-brand" href="/"><img className="logo" src="/assets/images/etcGradientTopDown.jpeg" alt="ETC Logo" width="30" height="30"></img></a>
-
-                                                <li><a href="/">Home</a></li>
-                                                        
-                                                <li><a href="#newsletter">Newsletter</a></li>
-                                                            
-                                                <li><a href="#media">Media</a></li>
-                                                        
-                                                <li><a href="#merchandise">Merchandise</a></li>
-                                            </ul>                      
-                                        </Navbar>
-                                    </Nav>
-                    </header>
+                            </Row> 
+                        <Row>
+                            <Col>                   
+                                <Nav className="main-nav">
+                                    <Navbar sticky="top">
+                                        <ul>
+                                            <a className="navbar-brand" href="/">
+                                                <img className="logo" src="/assets/images/etcGradientTopDown.jpeg" alt="ETC Logo" width="30" height="30"></img>
+                                            </a>
+                                            <li><a href="/">Home</a></li>            
+                                            <li><a href="#newsletter">Newsletter</a></li>
+                                            <li><a href="#media">Media</a></li>                                                    
+                                            <li><a href="#merchandise">Merchandise</a></li>
+                                        </ul>                      
+                                    </Navbar>
+                                </Nav>
+                            </Col> 
+                        </Row>
+                    </Container> 
+                </header>
             </>
         );
     }
--- src/components/Mission.js
+++ src/components/Mission.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Mission() {
     return (
-        <div>
-            <h2>Mission Statement</h2>
+        <div className="mission">
+            <div className="row row-content">
+                <div className="col text-center">
+                    <h2>Uplift - Empower - Elevate</h2>
+                    <blockquote>
+                        "We a;dslkfja;sldkfj asldkfja;lk ja;lk ;laksjdf;alksdjfa;lskdjfa ; laskdjf a;lskdfj asdfjoewifjz;vcok ;lskdjfa;lksdfjo;awiejf"
+                    </blockquote>
+                </div>
+            </div>
         </div>
     )
 }
