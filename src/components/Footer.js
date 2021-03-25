39097e9ec6b783cd09e3f0ed3af11668a7daa781
Marcus Edralin <medralin36@yahoo.com>
2021-03-24 18:22:39 -0700

    Footer Version 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,28 +5,32 @@ export default function Footer() {
     return (
         <div>
             <footer className="site-footer">
-                <div className="container">
-                    <div className="row">             
-                        <div className="col-4 col-sm-2 offset-1">
-                            <h5>Links</h5>
-                            <ul className="list-unstyled">
-                                <li><Link to='/home'>Home</Link></li>
-                                <li><Link to='/media'>Media</Link></li>
-                                <li><Link to='/store'>Store</Link></li>
-                                <li><Link to='/contactus'>Contact Us</Link></li>
-                            </ul>
-                        </div>
-                        <div className="col-6 col-sm-3 text-center">
-                        <h5>Social</h5>
-                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
-                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
-                        </div>
-                        <div className="col-sm-4 text-center">
-                        <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-555-555-1234</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co"><i className="fa fa-envelope-o" /> elevatetheculture@gmail.com</a>
-                        </div>
+                <div className="container text-center">
+                    <div className="brandAcronym">
+                        <h3>ETC</h3>
+                    </div>
+                    <div style={{ color: 'white' }}>
+                        <p>HANDCRAFTED IN THE USA</p>
+                    </div>
+                    <div style={{ padding: '10px' }}>
+                        <a role="button" className="btn btn-link" href="tel:+12065551234">888-888-8888</a>
+                    </div>
+                    <div style={{ paddingBottom: '35px' }}>
+                        <ul className="list-unstyled">
+                            <li><Link to='/products'>SHOP</Link></li>
+                            <li><Link to='/media'>MEDIA</Link></li>
+                            <li><Link to='/contactus'>CONTACT US</Link></li>
+                            <li><Link to='/home'>HOME</Link></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 style={{ color: '#00A59E' }}>ELEVATE WITH US</h4>
+                        <p>Get the latest updates on new products and promotions</p>
+                        <input
+                            style={{ backgroundColor: '#1d1b1b' }}
+                            placeholder="Your email address"
+                        />
+                        <button style={{ backgroundColor: 'green' }}>Go</button>
                     </div>
                 </div>
             </footer>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
-import React, { Component }  from 'react';
-import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, Col, Row, Container, NavbarBrand, NavbarToggler, Collapse, NavItem, Carousel, CarouselControl, CarouselIndicators } from 'reactstrap';
-
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Nav, Navbar, NavbarToggler, Collapse, NavItem, Carousel, CarouselControl, CarouselIndicators } from "reactstrap";
 
 // const carouselItems = [
 //     {
@@ -19,113 +18,109 @@ import { Nav, Navbar, Col, Row, Container, NavbarBrand, NavbarToggler, Collapse,
 // ]
 
 const Sale = () => {
-    return(
-        <div className='saleHeader d-flex'>
-                    <div className='col-5 justify-content-center' 
-                        style={{color: 'white'}}>
-                        <a href="/">
-                            <img className="logo" src="/assets/images/etcGradientTopDown.jpeg" alt="ETC Logo" width="30" height="30" />
-                        </a>
-                    </div>
-                    <div className='col justify-content-center' 
-                        style={{color: 'white'}}>
-                        <a href='/'>50% off SALE!!!</a>
-                    </div>
-        </div>
-    );
+  return (
+    <div className="saleHeader d-flex">
+      <div className="col-5 justify-content-center" style={{ color: "white" }}>
+        <a href="/">
+          <img className="logo" src="/assets/images/etcGradientTopDown.jpeg" alt="ETC Logo" width="30" height="30" />
+        </a>
+      </div>
+      <div className="col justify-content-center" style={{ color: "white" }}>
+        <a href="/">50% off SALE!!!</a>
+      </div>
+    </div>
+  );
 };
 
 const Banner = () => {
-    return(
-        <div className='banner'>
-            <div className='text-center'>
-                <div className='myContainer'>
-                    <div className='row'>
-                        <div className='col-sm-12'>
-                            <div className='bannerTextBg'>
-                                <h1 style={{color: 'black', fontWeight: '600'}}>Elevate the Culture</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="banner">
+      <div className="text-center">
+        <div className="myContainer">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="bannerTextBg">
+                <h1 style={{ color: "black", fontWeight: "600" }}>Elevate the Culture</h1>
+              </div>
             </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 class EtcNav extends Component {
-
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
     this.state = {
-        isNavOpen: false,
-        isModalOpen: false
+      isNavOpen: false,
+      isModalOpen: false,
     };
     this.toggleNav = this.toggleNav.bind(this);
+  }
+
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="mainNav sticky-top">
+          <Navbar dark expand="sm">
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <div className="container-fluid justify-content-center">
+                <div classsName="row">
+                  <Nav navbar>
+                    <NavItem>
+                      <NavLink className="nav-link col" to="/home" href="/" active>
+                        <i className="fa fa-home fa-md" /> Home
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className="nav-link col" to="/media" href="/">
+                        <i className="fa fa-film  fa-md" /> Media
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className="nav-link col" to="/products" href="/">
+                        <i className="fa fa-shopping-bag fa-md" /> Store
+                      </NavLink>
+                    </NavItem>
+                    <NavItem>
+                      <NavLink className="nav-link col" to="/contactus" href="/">
+                        <i className="fa  fa-address-card-o fa-md" /> Contact
+                      </NavLink>
+                    </NavItem>
+                  </Nav>
+                </div>
+              </div>
+            </Collapse>
+          </Navbar>
+        </div>
+        <div className="saleHeader" />
+      </>
+    );
+  }
 }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen 
-        });
-    }
-
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
-
-    render() {
-        return (
-            <>
-                <div className='mainNav sticky-top'>
-                    <Navbar dark expand="sm">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <div className='container-fluid justify-content-center'>
-                                <div classsName='row'>
-                                    <Nav navbar>
-                                        <NavItem>
-                                            <NavLink className="nav-link col" to="/home" href="/" active>
-                                                <i className="fa fa-home fa-md" /> Home
-                                            </NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink className="nav-link col" to="/media" href="/">
-                                                <i className="fa fa-film  fa-md" /> Media
-                                            </NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink className="nav-link col" to="/products" href="/">
-                                                <i className="fa fa-shopping-bag fa-md" /> Store
-                                            </NavLink>
-                                        </NavItem>
-                                        <NavItem>
-                                            <NavLink className="nav-link col" to="/contactus" href="/">
-                                               <i className="fa  fa-address-card-o fa-md" /> Contact
-                                            </NavLink>
-                                        </NavItem>
-                                    </Nav>
-                                </div>
-                            </div>
-                        </Collapse>
-                    </Navbar>   
-                </div>
-                <div className='saleHeader' />
-            </>
-        );
-    }
-};
-
-
 export default function Header() {
-    return (
-        <div>
-            <Sale/>
-            <EtcNav />
-            <Banner />
-        </div>
-    );
-};
+  return (
+    <div>
+      <Sale />
+      <EtcNav />
+      <Banner />
+    </div>
+  );
+}
